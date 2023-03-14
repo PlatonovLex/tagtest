c141b7fc74e03c3165d207226f8c7c2b073a97b9
Платонов Алексей <platonov.lex@gmail.com>
2023-03-14 22:44:08 +0300

    hotfix on hotfix - 2

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,6 @@
 public class MainClass {
     public static void main(String[] args) {
-        //Hotfix v1.1.1
-        System.out.println("The world is hell");
+        //Hotfix v1.1.2
+        System.out.println("The world is better then hell");
     }
 }
