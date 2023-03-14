8c811808ef775879a8f887449f221e8a2fab907f
Платонов Алексей <platonov.lex@gmail.com>
2023-03-14 22:35:29 +0300

    Feature - 4

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -7,4 +7,8 @@ public class MainClass {
     private static void newFeatureMethod() {
         //Feature 3 and conflict with fix
     }
+
+    private static void newFeature4Method() {
+        //Feature 4 and conflict with fix on fix
+    }
 }
