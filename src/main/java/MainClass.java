66f221648fa2f40f4e91a41e9a46de5b7d7095c2
Платонов Алексей <platonov.lex@gmail.com>
2023-03-14 22:23:29 +0300

    Feature with conflict - 3

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,9 @@
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world conflict");
+    }
+
+    private static void newFeatureMethod() {
+        //Feature 3 and conflict with fix
     }
 }
